75a93eec914f6d1d79a0c70a4c1372ebdf757016
pratik0007 <pratikgondane07@gmail.com>
2023-10-30 19:10:26 +0530

    lenovo_firmware

---
+++ lenovo-firmware/jd2019/firmware.mk
@@ -0,0 +1,18 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+	vendor/lenovo-firmware/jd2019/hyp.mbn:install/firmware-update/hyp.mbn \
+	vendor/lenovo-firmware/jd2019/xbl_config.elf:install/firmware-update/xbl_config.elf \
+	vendor/lenovo-firmware/jd2019/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+	vendor/lenovo-firmware/jd2019/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+	vendor/lenovo-firmware/jd2019/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+	vendor/lenovo-firmware/jd2019/devcfg.mbn:install/firmware-update/devcfg.mbn \
+	vendor/lenovo-firmware/jd2019/xbl.elf:install/firmware-update/xbl.elf \
+	vendor/lenovo-firmware/jd2019/keymaster64.mbn:install/firmware-update/keymaster64.mbn \
+	vendor/lenovo-firmware/jd2019/aop.mbn:install/firmware-update/aop.mbn \
+	vendor/lenovo-firmware/jd2019/dspso.bin:install/firmware-update/dspso.bin \
+	vendor/lenovo-firmware/jd2019/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+	vendor/lenovo-firmware/jd2019/tz.mbn:install/firmware-update/tz.mbn \
+	vendor/lenovo-firmware/jd2019/storsec.mbn:install/firmware-update/storsec.mbn \
+	vendor/lenovo-firmware/jd2019/BTFM.bin:install/firmware-update/BTFM.bin \
+	vendor/lenovo-firmware/jd2019/abl.elf:install/firmware-update/abl.elf
+
