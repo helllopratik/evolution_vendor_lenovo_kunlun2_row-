22ccaf8779ad8413755e57858c6ae1f46012cc13
pratik0007 <pratikgondane07@gmail.com>
2023-10-30 19:11:00 +0530

    lenovo_firmware

---
+++ lenovo-firmware/kunlun2/firmware.mk
@@ -0,0 +1,17 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+	vendor/lenovo-firmware/kunlun2/hyp.mbn:install/firmware-update/hyp.mbn \
+	vendor/lenovo-firmware/kunlun2/xbl_config.elf:install/firmware-update/xbl_config.elf \
+	vendor/lenovo-firmware/kunlun2/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+	vendor/lenovo-firmware/kunlun2/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+	vendor/lenovo-firmware/kunlun2/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+	vendor/lenovo-firmware/kunlun2/devcfg.mbn:install/firmware-update/devcfg.mbn \
+	vendor/lenovo-firmware/kunlun2/xbl.elf:install/firmware-update/xbl.elf \
+	vendor/lenovo-firmware/kunlun2/keymaster64.mbn:install/firmware-update/keymaster64.mbn \
+	vendor/lenovo-firmware/kunlun2/aop.mbn:install/firmware-update/aop.mbn \
+	vendor/lenovo-firmware/kunlun2/dspso.bin:install/firmware-update/dspso.bin \
+	vendor/lenovo-firmware/kunlun2/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+	vendor/lenovo-firmware/kunlun2/tz.mbn:install/firmware-update/tz.mbn \
+	vendor/lenovo-firmware/kunlun2/storsec.mbn:install/firmware-update/storsec.mbn \
+	vendor/lenovo-firmware/kunlun2/BTFM.bin:install/firmware-update/BTFM.bin \
+	vendor/lenovo-firmware/kunlun2/abl.elf:install/firmware-update/abl.elf
